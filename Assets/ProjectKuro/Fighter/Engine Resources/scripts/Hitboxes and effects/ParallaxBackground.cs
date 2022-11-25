e9dc6803ed8fb398f4c5918f0a5ecfc1d3386e5d
cameronsjlevine <chaotixlevine@gmail.com>
2022-11-25 15:37:53 -0600

    Parallax Background
    -Edited ParallaxBackground.cs in ProjectKuro/Fighter/Engine Resources/scripts/Hitboxes and effects
    	-Made it so the script actually works as intended and creates the parallax effect -- though due to the nature of the camera, it isn't very noticeable

--- Assets/ProjectKuro/Fighter/Engine Resources/scripts/Hitboxes and effects/ParallaxBackground.cs
+++ Assets/ProjectKuro/Fighter/Engine Resources/scripts/Hitboxes and effects/ParallaxBackground.cs
@@ -4,30 +4,31 @@ using UnityEngine;
 
 public class ParallaxBackground : MonoBehaviour
 {
-    private float length, startX, startY;
-    public GameObject camera;
-    public float parallaxEffect;
+    private float startX;
+    public GameObject gameCamera;
+    public float parallaxEffect; //The closer the object, the higher the value
+    private float cameraStartX;
 
     void Start()
     {
         startX = transform.position.x;
-        startY = transform.parent.transform.position.y;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;    
+        cameraStartX = gameCamera.transform.position.x;
     }
 
     void Update()
     {
-        float temp = (camera.transform.position.x * (1 - parallaxEffect));
-        float dist = (camera.transform.position.x * parallaxEffect);
+        float dist = ((Mathf.Abs(gameCamera.transform.position.x) - Mathf.Abs(cameraStartX)) * parallaxEffect);
 
-        transform.position = new Vector3(startX + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startX - dist, transform.position.y, transform.position.z);
+        
+        /*
         transform.parent.transform.position = new Vector3(transform.parent.transform.position.x, startY, transform.parent.transform.position.z);
-
         if(temp > startX + length){
             startX += length;
         }
         else if(temp < startX - length){
             startX -= length;
         }
+        */
     }
 }
