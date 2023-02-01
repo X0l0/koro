ccb48d6c442de20d05eb14f5db045fe246121529
X0l0 <jorge.a.villa@hotmail.com>
2023-01-31 23:57:52 -0700

    OWEnemy, Camera Adjustments
    -renamed ow enemy script from log to OWWIldKuro
    -deleted base Enemy script that log previously inherited from.
    -added comments and cleaned up OWWildkuro script.
    -adjusted some camera speeds and weights

---
+++ Assets/ProjectKuro/topdown/Scripts/Entities/OWWildKuro.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OWWildKuro : MonoBehaviour
+{
+    //this script is meant to handle wild kuros behaviour in the overworld
+
+    //add enum for various states?
+  //timid
+  //territorial
+  //stationary
+  //wandering
+  //curious
+  //predatory
+  //sleeping
+
+    //components
+    public Transform target;//a local transform variable meant to be filled in with the players transform.
+    public Transform homePosition;// a position on the map the wildkuro prefers to be stationed at
+
+    //Variables
+    public string enemyName;//simple string for enemy name, not currently in use.
+    public float MoveSpeed;//float for overworld move speed
+    public float chaseRadius;//radius where the wild kuro can sense the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckDistance();
+    }
+
+    void CheckDistance()
+    {
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > 1)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, MoveSpeed * Time.deltaTime);
+        }
+    }
+}
